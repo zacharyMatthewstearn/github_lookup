ce11c0df0048d6fa40d63f671bb6eb9e593315c7
Zachary Matthewstearn <zammo911@gmail.com>
2016-10-14 11:30:33 -0700

    output name and description for each repo of user

--- js/lookup.js
+++ js/lookup.js
@@ -1,13 +1,15 @@
 var apiKey = require('./../.env').apiKey;
 
-function Lookup() {
-
-}
-
 exports.getRepos = function(){
-  $.get('https://api.github.com/users/zacharyMatthewstearn?access_token=' + apiKey).then(function(response){
-    console.log(response);
+  $.get('https://api.github.com/users/zacharyMatthewstearn/repos?access_token=' + apiKey).then(function(response){
+    // console.log(response);
+    $('#output').html('');
+    for(var i = 0; i < response.length; i++){
+      // console.log(response[i]);
+      $('#output').append('<li><hr><p>' + response[i].name + '</p><p>' + response[i].description + '</p></li>');
+    }
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    // console.log(error.responseJSON.message);
+    alert(error.responseJSON.message);
   });
 };
